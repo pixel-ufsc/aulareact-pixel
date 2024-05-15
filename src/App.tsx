94cbf3a0f0e4a711b59b00162fbd9df6ef9d34b0
rlneto <rlneto@live.com>
2024-05-15 10:51:14 -0300

    Definindo visual

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 
 function App() {
+      {/* Identidade Visual da Pixel
+    Space Cadet: #82337E
+    Midnight-X: #023047
+    Sunrise-X: #FA8400
+    Midday-X: #1CACD8
+    #242837
+    #404045
+    #8F8F94
+    #DFDFE4*/}
 
   return (
     <>
-      <p className="flex justify-center text-4xl font-bold text-blue-500">Hello World</p>
+      <div className="flex flex-col w-full h-screen bg-gradient-to-b from-[#82337E] to-[#023047]">
+        <div className="flex flex-row justify-between items-center p-4 m-8 rounded-md shadow-md bg-[#DFDFE4]">
+          Teste
+        </div>
+      </div>
     </>
   )
 }
